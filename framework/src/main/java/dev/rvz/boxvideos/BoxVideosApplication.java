7c797cf5d91541f35b93b13ee4fc00dc905163cc
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-16 03:40:36 -0300

    Fix packages imports teste unit

--- framework/src/main/java/dev/rvz/boxvideos/BoxVideosApplication.java
+++ framework/src/main/java/dev/rvz/boxvideos/BoxVideosApplication.java
@@ -1,4 +1,4 @@
-package rvz.dev.boxvideos;
+package dev.rvz.boxvideos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- framework/src/test/java/dev/rvz/adapters/inbound/api/VideoRestControllerTest.java
+++ framework/src/test/java/dev/rvz/adapters/inbound/api/VideoRestControllerTest.java
@@ -1,15 +1,18 @@
-package rvz.dev.adapters.inbound.api;
+package dev.rvz.adapters.inbound.api;
 
+import dev.rvz.boxvideos.adapters.inbound.requests.videos.CreateVideoRequest;
+import dev.rvz.boxvideos.adapters.inbound.responses.videos.CreateVideoResponse;
+import dev.rvz.boxvideos.application.port.in.CreateVideoPortIn;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.http.MediaType;
 
 @WebMvcTest
 @AutoConfigureMockMvc
