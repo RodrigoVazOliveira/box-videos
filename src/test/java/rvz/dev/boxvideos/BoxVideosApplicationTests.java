bc7c475d8c8524b81813d59dc2c98857be5bb631
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-15 22:24:37 -0300

    Initial commit project Box Videos'

---
+++ src/test/java/rvz/dev/boxvideos/BoxVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package rvz.dev.boxvideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
