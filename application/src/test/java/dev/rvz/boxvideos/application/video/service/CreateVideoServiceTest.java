4907aae34f943528bfcf2de9f4bc42f4b825f00d
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-20 20:30:12 -0300

    create test to services domain

---
+++ application/src/test/java/dev/rvz/boxvideos/application/video/service/CreateVideoServiceTest.java
@@ -0,0 +1,33 @@
+package dev.rvz.boxvideos.application.video.service;
+
+import dev.rvz.boxvideos.core.domain.video.model.Video;
+import dev.rvz.boxvideos.port.in.CreateVideoPortIn;
+import dev.rvz.boxvideos.port.out.CreateVideoPortout;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CreateVideoServiceTest {
+
+    @Mock
+    private CreateVideoPortout createVideoPortout;
+
+    @InjectMocks
+    private CreateVideoService createVideoService;
+
+    @Test
+    void test_video_return_with_success() {
+        Video video = new Video(1L, "Testes 1", "Descrição", "http://meuvideo.comb.br");
+        Mockito.when(createVideoPortout.execute(Mockito.any())).thenReturn(video);
+        Video requestSaveVideo = new Video(1L, "Testes 1", "Descrição", "http://meuvideo.comb.br");
+        Video result = createVideoService.execute(requestSaveVideo);
+
+        Assertions.assertEquals(video, requestSaveVideo);
+    }
+
+}
---
+++ application/src/test/java/dev/rvz/boxvideos/application/video/service/GetAllVideoServiceTest.java
@@ -0,0 +1,37 @@
+package dev.rvz.boxvideos.application.video.service;
+
+import dev.rvz.boxvideos.core.domain.video.model.Video;
+import dev.rvz.boxvideos.port.out.GetAllVideosPortOut;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+
+@ExtendWith(MockitoExtension.class)
+class GetAllVideoServiceTest {
+
+    @Mock
+    private GetAllVideosPortOut getAllVideosPortOut;
+
+    @InjectMocks
+    private GetAllVideoService getAllVideoService;
+
+    @Test
+    void test_get_all_videos_with_success() {
+        Iterable<Video> allVideos = Arrays.asList(
+            new Video(1L, "Teste 1", "Video 1", "http://video1.com.br"),
+                new Video(1L, "Teste 2", "Video 2", "http://video2.com.br"),
+                new Video(1L, "Teste 3", "Video 3", "http://video3.com.br")
+        );
+
+        Mockito.when(getAllVideosPortOut.execute()).thenReturn(allVideos);
+        Iterable<Video> resultAllVideos = getAllVideoService.execute();
+
+        Assertions.assertEquals(allVideos, resultAllVideos);
+    }
+}
