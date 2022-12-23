00fcf3aadde639785cbd45b52a6e1fb0092e56cf
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-23 01:34:04 -0300

    Create test unit to update complete all video service

---
+++ application/src/test/java/dev/rvz/boxvideos/application/video/service/UpdateCompleteVideoServiceTest.java
@@ -0,0 +1,61 @@
+package dev.rvz.boxvideos.application.video.service;
+
+import dev.rvz.boxvideos.core.domain.video.model.Video;
+import dev.rvz.boxvideos.port.out.UpdateCompleteVideoPortOut;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UpdateCompleteVideoServiceTest {
+
+    @Test
+    void update_video_already_exists() {
+        Video video = new Video(1L, "Testes 1", "Descricao 1", "http://localhost");
+        UpdateCompleteVideoPortOut updateCompleteVideoPortOut = new UpdateCompleteVideoPortOut() {
+            @Override
+            public Video updateAlreadyExists(Video video) {
+                return video;
+            }
+
+            @Override
+            public Video createVideoIfNotExists(Video video) {
+                return null;
+            }
+
+            @Override
+            public Boolean existsVideoById(Long id) {
+                return true;
+            }
+        };
+
+        UpdateCompleteVideoService updateCompleteVideoService = new UpdateCompleteVideoService(updateCompleteVideoPortOut);
+        Video result = updateCompleteVideoService.execute(video);
+
+        Assertions.assertEquals(video, result);
+    }
+
+    @Test
+    void update_video_not_exists() {
+        Video video = new Video(1L, "Testes 1", "Descricao 1", "http://localhost");
+        UpdateCompleteVideoPortOut updateCompleteVideoPortOut = new UpdateCompleteVideoPortOut() {
+            @Override
+            public Video updateAlreadyExists(Video video) {
+                return null;
+            }
+
+            @Override
+            public Video createVideoIfNotExists(Video video) {
+                return video;
+            }
+
+            @Override
+            public Boolean existsVideoById(Long id) {
+                return false;
+            }
+        };
+
+        UpdateCompleteVideoService updateCompleteVideoService = new UpdateCompleteVideoService(updateCompleteVideoPortOut);
+        Video result = updateCompleteVideoService.execute(video);
+
+        Assertions.assertEquals(video, result);
+    }
+}
