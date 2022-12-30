aef8746a46544035a8b0d147ec996fd698c95f63
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-30 04:51:15 -0300

    Add query method

--- adapters/outbound/src/main/java/dev/rvz/boxvideos/adapters/outbound/repository/VideoRepository.java
+++ adapters/outbound/src/main/java/dev/rvz/boxvideos/adapters/outbound/repository/VideoRepository.java
@@ -1,7 +1,9 @@
 package dev.rvz.boxvideos.adapters.outbound.repository;
 
 import dev.rvz.boxvideos.adapters.commons.entity.VideoEntity;
+import dev.rvz.boxvideos.core.domain.video.model.Video;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VideoRepository extends CrudRepository<VideoEntity, Long> {
+    Iterable<Video> findByTitleContains(String title);
 }
