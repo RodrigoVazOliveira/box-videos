47d749f7ae297b26a30c9d19a68cec4893e41e5c
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-01-18 01:42:52 -0300

    Add banner and create new table

--- framework/src/main/java/dev/rvz/boxvideos/BoxVideosApplication.java
+++ framework/src/main/java/dev/rvz/boxvideos/BoxVideosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoxVideosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoxVideosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoxVideosApplication.class, args);
+    }
 
 }
