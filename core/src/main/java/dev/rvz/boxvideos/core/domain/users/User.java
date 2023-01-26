c7192a58726ea340eab8264d27c292f0f8d2268f
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-01-25 22:09:46 -0300

    Create entity user

--- core/src/main/java/dev/rvz/boxvideos/core/domain/users/User.java
+++
@@ -1,6 +0,0 @@
-package dev.rvz.boxvideos.core.domain.users;
-
-import dev.rvz.boxvideos.core.domain.users.enumerations.AccessRoleEnum;
-
-public record User(Long id, String name, String email, String password, AccessRoleEnum accessRole, Boolean active) {
-}
