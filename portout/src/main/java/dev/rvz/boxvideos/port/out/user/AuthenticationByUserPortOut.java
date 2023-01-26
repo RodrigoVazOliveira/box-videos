92bfdea3de493de68df1868b42a12fc262b325b1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-01-26 01:04:46 -0300

    remove methods

--- portout/src/main/java/dev/rvz/boxvideos/port/out/user/AuthenticationByUserPortOut.java
+++ portout/src/main/java/dev/rvz/boxvideos/port/out/user/AuthenticationByUserPortOut.java
@@ -1,11 +1,5 @@
 package dev.rvz.boxvideos.port.out.user;
 
-import dev.rvz.boxvideos.core.domain.users.model.User;
-
 public interface AuthenticationByUserPortOut {
     Boolean existsUserByEmailOrNick(String email, String nick);
-
-    String getPasswordDescrypted(String password);
-
-    User getUserByEmailOrNick(String email, String nick);
 }
